fc86ad3380d6581d0fc15f0e742b10fe14a336f0
King-Jide <jide.remote@gmail.com>
2025-09-24 11:01:24 +0100

    configure URL patterns for relationship_app

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,14 @@
+#relationship_app/urls.py
+from django.urls import path
+from . import views
+
+app_name = "relationship_app" #namespacing is good practice
+
+urlpatterns = [
+    #Function-based view route: list all books
+    path("books/", views.list_books, name="list_books"),
+
+    # Class-based view route: detail of a specific library by its pk
+    #Example: /library/1/ for library with pk= 1
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+]
